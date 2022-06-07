6624e3604205d6442ae3c16730d2a3f4dfe41ce7
emmaneely <emmaneely3@gmail.com>
2022-06-07 13:39:59 -0500

    Completed exercise 2

--- app.js
+++ app.js
@@ -16,11 +16,46 @@ let multiply = a * b;
   console.log(multiply);
 let dividing = a / b;
   console.log(dividing);
-  
+
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+let numstr = num + str;
+  console.log(numstr);
+  // What is the value of: num + str?
+  // Answer: 1111
+
+let numstr2 = num + str2;
+  console.log(numstr2);
+  // What is the value of: num + str2?
+  // Answer: 11eleven
+
+let numIsPresent = num + isPresent;
+  console.log(numIsPresent);
+  // What is the value of: num + isPresent?
+  // Answer: 12
+
+let firstNameNum = firstName + num;
+  console.log(firstNameNum);
+  // What is the value of: firstName + num?
+  // Answer: Frodo11
+
+let isPresentStr= isPresent + str;
+  console.log(isPresentStr);
+  // What is the value of: isPresent + str?
+  // Answer: true11
+
+let firstNameLastName = firstName + lastName;
+  console.log(firstNameLastName);
+  // What is the value of: firstName + lastName?
+  // Answer: FrodoBaggins
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
